19c4b6e3fca4fdd823e57f7cbf371a0a35cb9ace
thiagolz <thiagozarate7@outlook.com>
2022-02-07 10:20:47 -0300

    Display-Product-Detail

--- src/components/Products/ItemDetail.js
+++ src/components/Products/ItemDetail.js
@@ -1,11 +1,11 @@
-import ItemDetailCss from "./ItemDetail.css";
+import "./ItemDetail.css";
 import { Box } from "@chakra-ui/react";
 export default function ItemDetail({ item }) {
   return (
-    <Box bgGradient="w='100%' h='200px' bgGradient='linear(to-r, green.200, pink.500)'">
-      <div className="products-container">
+    <Box bgGradient="linear(red.100 20%, orange.50 5%, yellow.100 50%)">
+      <div className="Item-Detail-container">
         <img src={item.image} alt="Product Image" />
-        <div className="cards-container">
+        <div className="card-container">
           <p className="price">$ {item.price}</p>
           <p className="title">{item.title}</p>
           <p className="description">{item.description}</p>
--- src/components/Products/ItemDetailContainer.js
+++ src/components/Products/ItemDetailContainer.js
@@ -2,20 +2,23 @@ import { Box } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import { getItems } from "../../data/data";
 import ItemDetail from "./ItemDetail";
-
+import ItemDetailContainerCss from "./ItemDetailContainer.css";
 export default function ItemDetailContainer({ item }) {
   const [Getproduct, setGetProduct] = useState([]);
   //   const [loading, setLoading] = useState(true);
   useEffect(() => {
     getItems().then((item) => {
-      setGetProduct(item);
       console.log(item);
-      item.find((item) => item.id === 1);
+      const data = item.find((item) => item.id === 1);
+      setGetProduct(data);
     });
   }, []);
+
   return (
-    <div>
-      <ItemDetail item={Getproduct} />
+    <div className="item-detail-container ">
+      <Box>
+        <ItemDetail item={Getproduct} />
+      </Box>
     </div>
   );
 }
